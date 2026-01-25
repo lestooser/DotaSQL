060d64c24b9e78349bd06f69527912b21219a531
lestooser <71587682+lestooser@users.noreply.github.com>
2026-01-26 02:55:17 +0300

    Перевод даты старта

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 PLAYER_ID = '327729645'
 
@@ -10,13 +11,22 @@ def fetch_match(player_id):
     
     if response.status_code != 200:
         raise Exception("API request failed with status code: {}".format(response.status_code))
-    
     return response.json()
+
+def format_timestamp(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+def matches_formating(matches):
+    for match in matches:
+        if 'duration' in match:
+            match['duration'] = match['duration'] // 60  # Convert seconds to minutes
+        if 'start_time' in match:
+            match['start_time'] = format_timestamp(match['start_time'])
 
 if __name__ == "__main__":
     try:
         matches = fetch_match(PLAYER_ID)
-        [match.__setitem__('duration', match['duration'] // 60) for match in matches if 'duration' in match]
+        matches_formating(matches)
         print(json.dumps(matches[:5], indent=2))
     except Exception as e:
         print(f"Error fetching matches: {e}")
